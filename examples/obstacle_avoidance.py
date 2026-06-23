af4e979204e74aa550e492623578100726bd5da5
Lilly <lillyschwarzenbach@gmail.com>
2026-06-23 17:19:21 +0200

    centralize runner

--- examples/obstacle_avoidance.py
+++ examples/obstacle_avoidance.py
@@ -2,7 +2,7 @@ import asyncio
 
 from swarm_platform.command import RobotCommand
 from swarm_platform.controller import Controller 
-from swarm_platform import Robot
+from swarm_platform.runner import Runner
 
 class ObstacleAvoidance(Controller):
 
@@ -26,36 +26,6 @@ class ObstacleAvoidance(Controller):
             right_motor=self.speed,
             top_led=(0, 32, 0),
         )
-    
-
-class Runner:
-
-    def __init__(self, controller, frequency=20):
-
-        self.controller = controller
-        self.dt = 1 / frequency
-
-    async def run(self):
-
-        async with Robot() as robot:
-
-            await self.controller.setup(robot)
-
-            try:
-
-                while True:
-
-                    state = await robot.state()
-
-                    command = await self.controller.step(state)
-
-                    await robot.apply(command)
-
-                    await asyncio.sleep(self.dt)
-
-            finally:
-
-                await self.controller.cleanup(robot)
 
 
 async def main():
