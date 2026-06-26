c417a04b17a321b083dd15c0b6066d7b82267946
Lilly <lillyschwarzenbach@gmail.com>
2026-06-26 12:48:40 +0200

    add time fix to platform setup

--- swarm_platform_setup.sh
+++ swarm_platform_setup.sh
@@ -6,6 +6,8 @@ python3 -m venv .venv
 
 source .venv/bin/activate
 
+sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
+
 python -m pip install --upgrade pip
 
 pip install -e .
