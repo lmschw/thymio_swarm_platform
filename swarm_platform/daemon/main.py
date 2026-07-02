c89da4c8987b0ecfed42052096a2f16916f15d4d
Lilly <lillyschwarzenbach@gmail.com>
2026-07-02 15:34:04 +0200

    debug script change

--- swarm_platform/daemon/main.py
+++ swarm_platform/daemon/main.py
@@ -3,8 +3,12 @@ from swarm_platform.daemon.server import SwarmDaemon
 
 
 def main():
+    print("MAIN ENTRYPOINT REACHED")
     daemon = SwarmDaemon()
     asyncio.run(daemon.run())
+
+
+print("SCRIPT LOADED")
 
 
 if __name__ == "__main__":
