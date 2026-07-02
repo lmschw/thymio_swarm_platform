c25e42cf7dceb83ac11d6e1a5b271deb28b49e67
Lilly <lillyschwarzenbach@gmail.com>
2026-07-02 10:52:52 +0200

    add date update to swarm setup script

--- scripts/swarm_platform_setup.sh
+++ scripts/swarm_platform_setup.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
+
 echo "===== Swarm Platform Setup ====="
 
 PROJECT_DIR="$(pwd)"
