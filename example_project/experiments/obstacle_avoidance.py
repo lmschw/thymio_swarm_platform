219d3e2b4ef15243a2464fa073284d476ed6cd97
Lilly <lillyschwarzenbach@gmail.com>
2026-07-03 11:06:13 +0200

    readd removed code

--- example_project/experiments/obstacle_avoidance.py
+++ example_project/experiments/obstacle_avoidance.py
@@ -1,65 +1,82 @@
 import asyncio
 
 
-async def run(self):
-    print("OBSTACLE AVOIDANCE STARTED", flush=True)
-    print("[EXPERIMENT INSTANCE]", id(self), flush=True)
-    print("[LOGGER ON INSTANCE]", getattr(self, "logger", None), flush=True)
+class ObstacleAvoidance:
 
-    try:
-        while self.running:
+    name = "obstacle_avoidance"
 
-            if self.paused:
-                await self.robot.stop()
-                await asyncio.sleep(0.1)
-                continue
+    FORWARD_SPEED = 200
+    TURN_SPEED = 150
+    THRESHOLD = 1800
 
-            prox = await self.robot.proximity_horizontal()
+    def __init__(self, robot, config=None, logger=None):
+        self.robot = robot
+        self.config = config or {}
+        self.logger = logger 
 
-            left = prox[0] + prox[1]
-            center = prox[2]
-            right = prox[3] + prox[4]
+        self.running = True
+        self.paused = False
 
-            if center > self.THRESHOLD:
 
-                await self.robot.top_led(255, 255, 0)
+    async def run(self):
+        print("OBSTACLE AVOIDANCE STARTED", flush=True)
+        print("[EXPERIMENT INSTANCE]", id(self), flush=True)
+        print("[LOGGER ON INSTANCE]", getattr(self, "logger", None), flush=True)
 
-                if left < right:
-                    left_speed = -self.TURN_SPEED
-                    right_speed = self.TURN_SPEED
+        try:
+            while self.running:
+
+                if self.paused:
+                    await self.robot.stop()
+                    await asyncio.sleep(0.1)
+                    continue
+
+                prox = await self.robot.proximity_horizontal()
+
+                left = prox[0] + prox[1]
+                center = prox[2]
+                right = prox[3] + prox[4]
+
+                if center > self.THRESHOLD:
+
+                    await self.robot.top_led(255, 255, 0)
+
+                    if left < right:
+                        left_speed = -self.TURN_SPEED
+                        right_speed = self.TURN_SPEED
+                    else:
+                        left_speed = self.TURN_SPEED
+                        right_speed = -self.TURN_SPEED
+
                 else:
-                    left_speed = self.TURN_SPEED
-                    right_speed = -self.TURN_SPEED
+                    await self.robot.top_led(0, 255, 0)
+                    left_speed = self.FORWARD_SPEED
+                    right_speed = self.FORWARD_SPEED
 
-            else:
-                await self.robot.top_led(0, 255, 0)
-                left_speed = self.FORWARD_SPEED
-                right_speed = self.FORWARD_SPEED
+                await self.robot.drive(left_speed, right_speed)
 
-            await self.robot.drive(left_speed, right_speed)
+                if self.logger:
+                    self.logger.log(
+                        state={"proximity": prox},
+                        command={
+                            "left": left_speed,
+                            "right": right_speed,
+                        }
+                    )
+
+                await asyncio.sleep(0.05)
+
+        except asyncio.CancelledError:
+            print("[EXPERIMENT] Cancelled safely", flush=True)
+
+        finally:
+            await self.robot.stop()
+            await self.robot.top_led(0, 0, 0)
 
             if self.logger:
                 self.logger.log(
-                    state={"proximity": prox},
-                    command={
-                        "left": left_speed,
-                        "right": right_speed,
-                    }
+                    state={"event": "shutdown"},
+                    command=None
                 )
 
-            await asyncio.sleep(0.05)
-
-    except asyncio.CancelledError:
-        print("[EXPERIMENT] Cancelled safely", flush=True)
-
-    finally:
-        await self.robot.stop()
-        await self.robot.top_led(0, 0, 0)
-
-        if self.logger:
-            self.logger.log(
-                state={"event": "shutdown"},
-                command=None
-            )
-
-            self.logger.close()
+                self.logger.close()
